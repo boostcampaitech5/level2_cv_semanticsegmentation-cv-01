3d09d969c99ed635b1f2a8fe61b316dfd8270978
Hyeji0828 <janghyeji0828@gmail.com>
2023-06-11 03:10:32 +0000

    add deeplabv3

--- baseline/models.py
+++ baseline/models.py
@@ -9,3 +9,9 @@ def fcn_resnet50(class_len):
     # output class를 data set에 맞도록 수정
     model.classifier[4] = nn.Conv2d(512, class_len, kernel_size=1)
     return model
+
+def deeplabv3(class_len):
+    model = models.segmentation.deeplabv3_resnet50(pretrained=True)
+    
+    model.classifier = models.segmentation.deeplabv3.DeepLabHead(2048, class_len)
+    return model
