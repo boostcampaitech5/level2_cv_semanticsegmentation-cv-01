e5a7af5eef97d126590896ce053e7af4cd05d775
Hyeji0828 <janghyeji0828@gmail.com>
2023-06-12 06:55:43 +0000

    add tversky loss

--- baseline/loss.py
+++ baseline/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 
 def focal_loss(pred, gt, alpha=.25, gamma=2):
     pred = F.sigmoid(pred)
@@ -28,12 +29,26 @@ def IoU_loss(pred, gt, smooth=1):
     IoU = (intersection + smooth) / (union + smooth)
     return 1 - IoU
 
-# TODO Tveersky Loss
+# TODO Tversky Loss
+class TverskyLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, pred, gt, smooth=1, alpha=0.5, beta=0.5):
+        pred = pred.view(-1)
+        gt = gt.view(-1)
+
+        TP = (pred * gt).sum()
+        FP = ((1-gt) * pred).sum()
+        FN = (gt * (1-pred)).sum()
+
+        loss = (TP + smooth) / (TP + alpha * FP + beta * FN + smooth)
+        return 1 - loss
 
 def weighted_sum_loss(pred, gt, weight=0.5):
     bce = F.binary_cross_entropy_with_logits(pred, gt)
     pred = F.sigmoid(pred)
-    dice = dice_loss(pred, gt)
+    dice = dice_loss(pred, gt, 0.2)
     loss = bce * weight + dice * (1-weight)
 
     return loss
